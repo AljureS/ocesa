33d42f1744da9c763725f39c8280cb92148e0a07
AljureS <saidsimon2@gmail.com>
2024-12-23 18:01:26 -0500

    Nav Bar web not responsive

--- ocesa/src/components/NavBar.jsx
+++ ocesa/src/components/NavBar.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // Importar Link
+import './NavBar.css';
 
 const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const openMenu = ()=> {
+        setIsOpen(!isOpen)
+    }
+
     return (
-        
         <nav className="navbar">
             <div className="navbar-container">
-                <h1 className="logo">Mi Sitio</h1>
-                <ul className="menu">
-                <li><Link to="/">Inicio</Link></li>
-                <li><Link to="/about">Acerca</Link></li>
-                <li><Link to="/services">Servicios</Link></li>
-                <li><Link to="/contact">Contacto</Link></li>
+                <img src="./og/ocesa.png" alt="logo-ocesa" className="logo" />
+                {/* <h1 className="logo">Ocesa</h1> */}
+                <button 
+                    className="menu-toggle" 
+                    onClick={openMenu} 
+                    style={{ display: window.innerWidth <= 768 ? 'block' : 'none' }}
+                    >
+                    ☰
+                </button>
+
+                <ul className={`menu ${isOpen ? 'menu-open' : ''}`} style={{ display: window.innerWidth > 768 ? 'flex' : isOpen ? 'flex' : 'none' }}>
+                    <li><Link to="/">INICIO</Link></li>
+                    {/* <li><Link to="/novedades">Novedades</Link></li> */}
+                    {/* <li><Link to="/events">Eventos</Link></li> */}
+                    {/* <li><Link to="/galery">Galeria</Link></li> */}
+                    {/* <li><Link to="/greenDistrict">VERDE</Link></li> */}
+                    <li><Link to="/nosotros">NOSOTROS</Link></li>
                 </ul>
             </div>
         </nav>
-        
     );
 };
 
 export default NavBar;
+
