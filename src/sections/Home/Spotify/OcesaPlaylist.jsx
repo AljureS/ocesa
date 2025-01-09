81066ed6095d5e96b59934f166c47ee54cfde4f8
AljureS <saidsimon2@gmail.com>
2025-01-09 12:33:33 -0500

    Se mejoro

--- src/sections/Home/Spotify/OcesaPlaylist.jsx
+++ src/sections/Home/Spotify/OcesaPlaylist.jsx
@@ -3,6 +3,12 @@ import React from 'react'
 const OcesaPlaylist = () => {
     return (
         <>
+        <div>
+            <iframe style={{ borderRadius: '12px' }} src="https://open.spotify.com/embed/playlist/4pZ2RieXjMWURPCUfij6ew?utm_source=generator" width="100%" height="352" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <div>
+                
+            </div>
+        </div>
         </>
     )
 }
