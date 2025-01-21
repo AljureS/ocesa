7b67b5375722e03b9c4cfc763ae153c6581c34c2
AljureS <saidsimon2@gmail.com>
2025-01-21 15:59:30 -0500

    Aboust us full page

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -8,7 +8,7 @@ const AboutUs = () => {
             <section className='about-us-1'>
                 <div  className='about-us-container-1'>
                     <img className='concierto' src="aboutUs/concierto.jpg" alt="concierto" />
-                    <h3 id='title-about' className='animate__animated animate__slideInDown'>¿Quiénes somos?</h3>
+                    <h3 id='title-about' className='animate__animated animate__slideInDown animate__delay-0.5s'>¿Quiénes somos?</h3>
                 </div>
             </section>
 
