23eb33eebdf67551fb6e5c15de88224dad4f23b2
AljureS <saidsimon2@gmail.com>
2025-01-21 15:51:49 -0500

    Pagina de nosotros completada full farra full fiesta

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -32,40 +32,42 @@ const AboutUs = () => {
             </section>
 
             <section className='contact'>
-            
-                <div className="outer">
-                    <div className="dot"></div>
-                    <div className="card">
-                        <div className="ray"></div>
-                        <div className="text">
-                            <div className="contact-details">
-                                <h2 className="section-header">Contacto</h2>
+                <ScrollAnimation animateIn="animate__flipInX" animateOut='animate__fadeOutUpBig'  animateOnce={false}>
+                    <div className="outer">
+                        <div className="dot"></div>
+                        <div className="card">
+                            <div className="ray"></div>
+                            <div className="text">
+                                <div className="contact-details">
+                                    <h2 className="section-header">Contacto</h2>
 
-                                <div className="direct-contact-container">
-                                    <ul className="contact-list">
-                                        <li className="list-item"><span className="contact-text place">Cra 11 # 87 - 51 of 201, Bogotá - Colombia</span></li>
-                                        <li className="list-item"><span className="contact-text phone"><a href="tel:+576017940300" title="Llámanos">+57 601 794 0300</a></span></li>
-                                        <li className="list-item"><span className="contact-text gmail"><a href="mailto:notificaciones-col@ocesa.co" title="Envíanos un correo">notificaciones-col@ocesa.co</a></span></li>
-                                    </ul>
+                                    <div className="direct-contact-container">
+                                        <ul className="contact-list">
+                                            <li className="list-item"><span className="contact-text place">Cra 11 # 87 - 51 of 201, Bogotá - Colombia</span></li>
+                                            <li className="list-item"><span className="contact-text phone"><a href="tel:+576017940300" title="Llámanos">+57 601 794 0300</a></span></li>
+                                            <li className="list-item"><span className="contact-text gmail"><a href="mailto:notificaciones-col@ocesa.co" title="Envíanos un correo">notificaciones-col@ocesa.co</a></span></li>
+                                        </ul>
+                                    </div>
+
+                                    <div className="copyright">
+                                        <div className="logo-contact">
+                                            {/* <a href="https://ocesa.co/"> */}
+                                            <img className="logoContact" src="og/ocesa.webp" alt="Ocesa Colombia" width="100"/>
+                                            {/* </a> */}
+                                        </div> 
+                                        © COLOMBIA DERECHOS RESERVADOS
+                                    </div>
+
                                 </div>
-
-                                <div className="copyright">
-                                    <div className="logo-contact">
-                                        {/* <a href="https://ocesa.co/"> */}
-                                        <img className="logoContact" src="og/ocesa.webp" alt="Ocesa Colombia" width="100"/>
-                                        {/* </a> */}
-                                    </div> 
-                                    © COLOMBIA DERECHOS RESERVADOS
-                                </div>
-
                             </div>
+                            <div className="line topl"></div>
+                            <div className="line leftl"></div>
+                            <div className="line bottoml"></div>
+                            <div className="line rightl"></div>
                         </div>
-                        <div className="line topl"></div>
-                        <div className="line leftl"></div>
-                        <div className="line bottoml"></div>
-                        <div className="line rightl"></div>
                     </div>
-                </div>
+
+                </ScrollAnimation>
 
             </section>
 
