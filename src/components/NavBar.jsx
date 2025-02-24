83995a674d8c60af5a448b3b737e80f4f39360a2
AljureS <saidsimon2@gmail.com>
2025-02-24 18:25:20 -0500

    siuu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { NavLink, useNavigate, useLocation, Link } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 import "./NavBar.css";
 import { GrClose } from "react-icons/gr";
 import { RiMenuUnfold4Fill } from "react-icons/ri";
