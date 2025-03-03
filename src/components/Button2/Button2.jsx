29d93ad00b696ad2a752723fcca040bf9e769053
AljureS <saidsimon2@gmail.com>
2025-03-03 16:57:02 -0500

    Jurrasic

--- src/components/Button2/Button2.jsx
+++ src/components/Button2/Button2.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-
 import PropTypes from 'prop-types'
 import './Button2.css'
 
-
 const Button2 = ({ botonInfo }) => {
     const {url, btnText, availability} = botonInfo
+    console.log(btnText);
+    
     return (
-        <button className="button" data-text="Awesome" disabled={!availability} onClick={() => window.open(url, "_blank")}>
+        <button className="button" data-text={btnText} disabled={!availability} onClick={() => window.open(url, "_blank")}>
             <span className="actual-text">&nbsp;{btnText}&nbsp;</span>
             {
-                !availability ? true :  <span aria-hidden="true" className="hover-text">&nbsp;{btnText}&nbsp;</span>
+                !availability ? true :  <span aria-hidden="true" data-text={btnText} className="hover-text">&nbsp;{btnText}&nbsp;</span>
             }
         </button>
     )
